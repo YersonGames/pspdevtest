0c7fa0fa893e51cc8cfe6a2f17dab0a87e239d19
YersonGames <pirno98k@gmail.com>
2026-08-12 01:55:42 -0400

    raylib cmake config

---
+++ main2.cpp
@@ -0,0 +1,27 @@
+#include <pspkernel.h>
+#include <pspdisplay.h>
+#include <pspdebug.h>
+#include <pspctrl.h>
+#include <raylib.h>
+
+PSP_MODULE_INFO("test", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
+
+//SceCrtlData pad;
+
+int main(void)
+{
+    InitWindow(480,272,"Hola");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Hola",0,0,16,WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
