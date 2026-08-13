7eab54ac8c9773bd2bdba5bf4462734ae16a95e1
YersonGames <49105974+YersonGames@users.noreply.github.com>
2026-08-12 22:59:06 -0400

    Delete build directory

--- build/CMakeFiles/hello.dir/src/Player.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/hello.dir/src/Player.cpp.obj: \
- /home/yerson/repos/pspdevtest/src/Player.cpp
--- build/CMakeFiles/pspGameTest.dir/src/Player.cpp.obj.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/pspGameTest.dir/src/Player.cpp.obj: \
- /home/yerson/repos/pspdevtest/src/Player.cpp \
- /home/yerson/pspdev/psp/include/raylib.h \
- /home/yerson/pspdev/lib/gcc/psp/15.2.0/include/stdarg.h \
- /home/yerson/pspdev/psp/sdk/include/pspctrl.h \
- /home/yerson/repos/pspdevtest/src/Player.h \
- /home/yerson/repos/pspdevtest/src/PlayerVariables.h
