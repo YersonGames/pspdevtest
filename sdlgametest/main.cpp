5a29c66fd037fb2c448898ae1baa933ec0ba0088
YersonGames <pirno98k@gmail.com>
2026-08-16 03:20:56 -0400

    sdl, por ahora, no funca en ppsspp creo, prueba psp fisica

--- sdlgametest/main.cpp
+++ sdlgametest/main.cpp
@@ -7,18 +7,54 @@
 #include <pspuser.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <SDL3_mixer/SDL_mixer.h>
+//#include <SDL3_mixer/SDL_mixer.h>
 #include <string>
-
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
 
-    while (true)
-    {
+    SDL_Window* gWindow = nullptr;
+    SDL_Renderer* gRender = nullptr;
 
+    if(SDL_Init(SDL_INIT_VIDEO) == false) 
+    {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
     }
+
+    gWindow = SDL_CreateWindow("window",480,272,0);
+    gRender = SDL_CreateRenderer(gWindow,nullptr);
+
+    if (gWindow == nullptr || gRender == nullptr)
+    {
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_FRect square = {216, 96, 34, 64}; 
+    bool exitWindow = false;
+
+    while (exitWindow == false)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                exitWindow = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(gRender,255,255,255,255);
+        SDL_RenderFillRect(gRender,&square);
+        SDL_RenderPresent(gRender);
+    }
+
+    SDL_DestroyWindow(gWindow);
+    SDL_DestroyRenderer(gRender);
+    SDL_Quit();
     return 0;
 }
